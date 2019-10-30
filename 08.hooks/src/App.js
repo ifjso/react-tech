cd9428dbab59396e2d34c6b948357a9321eeb7d3
ifjso <ojsboy@hotmail.com>
2019-10-31 01:13:23 +0900

    useEffect 추가

--- 08.hooks/src/App.js
+++ 08.hooks/src/App.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Info from './Info';
 
-const App = () => <Info />;
+const App = () => {
+	const [ visible, setVisible ] = useState(false);
+
+	return (
+		<div>
+			<button onClick={() => setVisible(!visible)}>{visible ? '숨기기' : '보이기'}</button>
+			<hr />
+			{visible && <Info />}
+		</div>
+	);
+};
 
 export default App;
