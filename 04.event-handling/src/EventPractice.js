1e05f7dd75df0b2d50aecbd6be572b981076ae22
ifjso <ojsboy@hotmail.com>
2019-11-09 16:22:21 +0900

    이벤트 메서드로 분리

--- 04.event-handling/src/EventPractice.js
+++ 04.event-handling/src/EventPractice.js
@@ -5,6 +5,19 @@ class EventPractice extends Component {
     message: ""
   };
 
+  handleChange = e => {
+    this.setState({
+      message: e.target.value
+    });
+  };
+
+  handleClick = () => {
+    alert(this.state.message);
+    this.setState({
+      message: ""
+    });
+  };
+
   render() {
     return (
       <div>
@@ -14,20 +27,9 @@ class EventPractice extends Component {
           name="message"
           placeholder="아무가나 입력해 보세요"
           value={this.state.message}
-          onChange={e => {
-            this.setState({
-              message: e.target.value
-            });
-          }}
+          onChange={this.handleChange}
         />
-        <button
-          onClick={() => {
-            alert(this.state.message);
-            this.setState({ message: "" });
-          }}
-        >
-          확인
-        </button>
+        <button onClick={this.handleClick}>확인</button>
       </div>
     );
   }
