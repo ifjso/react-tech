2c2ca729d67b553f3396500f8c2f5ed5c5e250ab
ifjso <ojsboy@hotmail.com>
2020-01-05 21:44:24 +0900

    immer 적용

--- 11.immer-tutorial/src/App.js
+++ 11.immer-tutorial/src/App.js
@@ -1,4 +1,5 @@
 import React, { useRef, useCallback, useState } from 'react';
+import produce from 'immer';
 
 const App = () => {
   const nextId = useRef(1);
@@ -8,10 +9,11 @@ const App = () => {
   const onChange = useCallback(
     e => {
       const { name, value } = e.target;
-      setForm({
-        ...form,
-        [name]: value
-      });
+      setForm(
+        produce(form, draft => {
+          draft[name] = value;
+        })
+      );
     },
     [form]
   );
@@ -25,10 +27,11 @@ const App = () => {
         username: form.username
       };
 
-      setData({
-        ...data,
-        array: data.array.concat(info)
-      });
+      setData(
+        produce(data, draft => {
+          draft.array.push(info);
+        })
+      );
 
       setForm({
         name: '',
@@ -42,7 +45,14 @@ const App = () => {
 
   const onRemove = useCallback(
     id => {
-      setData({ ...data, array: data.array.filter(info => info.id !== id) });
+      setData(
+        produce(data, draft => {
+          draft.array.splice(
+            draft.array.findIndex(info => info.id === id),
+            1
+          );
+        })
+      );
     },
     [data]
   );
