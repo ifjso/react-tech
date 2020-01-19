c3c6aa81398222e0d5784f3ca17030af8e20b1c5
ifjso <ojsboy@hotmail.com>
2020-01-19 15:37:50 +0900

    categories component 추가

--- 14.news-viewer/src/App.js
+++ 14.news-viewer/src/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import NewsList from './components/NewsList';
+import Categories from './components/Categories';
 
-const App = () => <NewsList />;
+const App = () => {
+  return (
+    <>
+      <Categories />
+      <NewsList />
+    </>
+  );
+};
 
 export default App;
