1afe3fb8097f834051c65279c47aadad58d4be41
ifjso <ojsboy@hotmail.com>
2020-01-19 16:02:28 +0900

    카테고리에 따라 api 호출 추가

--- 14.news-viewer/src/components/NewsList.js
+++ 14.news-viewer/src/components/NewsList.js
@@ -16,7 +16,7 @@ const NewsListBlock = styled.div`
   }
 `;
 
-const NewsList = () => {
+const NewsList = ({ category }) => {
   const [articles, setArticles] = useState(null);
   const [loading, setLoading] = useState(false);
 
@@ -24,8 +24,9 @@ const NewsList = () => {
     const fetchData = async () => {
       setLoading(true);
       try {
+        const query = category === 'all' ? '' : `&category=${category}`;
         const response = await axios.get(
-          'https://newsapi.org/v2/top-headlines?country=kr&apiKey=5b6b640ef57843c6994db7b218608b29',
+          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=5b6b640ef57843c6994db7b218608b29`,
         );
         setArticles(response.data.articles);
       } catch (e) {
@@ -34,7 +35,7 @@ const NewsList = () => {
       setLoading(false);
     };
     fetchData();
-  }, []);
+  }, [category]);
 
   if (loading) {
     return <NewsListBlock>대기 중...</NewsListBlock>;
