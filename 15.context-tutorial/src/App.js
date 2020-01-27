a3c0f47d6adeff72410dbc7387b0b4f60e4b03e2
ifjso <ojsboy@hotmail.com>
2020-01-28 01:04:19 +0900

    SelectColors component 추가

--- 15.context-tutorial/src/App.js
+++ 15.context-tutorial/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ColorBox from './components/ColorBox';
 import { ColorProvider } from './contexts/color';
+import SelectColors from './components/SelectColors';
 
 const App = () => (
   <ColorProvider>
     <div>
+      <SelectColors />
       <ColorBox />
     </div>
   </ColorProvider>
