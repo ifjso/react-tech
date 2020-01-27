745780605aa108669cf05bcbd6b75705097dd826
ifjso <ojsboy@hotmail.com>
2020-01-27 14:13:38 +0900

    Context.Provider 적용

--- 15.context-tutorial/src/App.js
+++ 15.context-tutorial/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ColorBox from './components/ColorBox';
+import ColorContext from './contexts/color';
 
 const App = () => (
-  <div>
-    <ColorBox />
-  </div>
+  <ColorContext.Provider value={{ color: 'red' }}>
+    <div>
+      <ColorBox />
+    </div>
+  </ColorContext.Provider>
 );
 
 export default App;
