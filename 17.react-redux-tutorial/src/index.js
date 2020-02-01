5f078dd44ed6b3df5f0bd3e2e3f69a59a1667756
ifjso <ojsboy@hotmail.com>
2020-02-01 18:13:01 +0900

    store 생성

--- 17.react-redux-tutorial/src/index.js
+++ 17.react-redux-tutorial/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import rootReducers from './modules';
+
+const store = createStore(rootReducers);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
