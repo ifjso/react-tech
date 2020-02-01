21559b2876c0d5c3550f7348df236a1c8b320c33
ifjso <ojsboy@hotmail.com>
2020-02-01 15:47:04 +0900

    리듀서 추가

--- 16.vanilla-redux/index.js
+++ 16.vanilla-redux/index.js
@@ -15,3 +15,16 @@ const initialState = {
   toggle: false,
   counter: 0
 };
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case TOGGLE_SWITCH:
+      return { ...state, toggle: !state.toggle };
+    case INCREASE:
+      return { ...state, counter: state.counter + action.difference };
+    case DECREASE:
+      return { ...state, counter: state.counter - 1 };
+    default:
+      return state;
+  }
+};
