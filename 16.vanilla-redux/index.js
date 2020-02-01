584c273a2c3870bb784e8800b8371d3328251375
ifjso <ojsboy@hotmail.com>
2020-02-01 15:39:37 +0900

    액션 함수 및 초깃값 추가

--- 16.vanilla-redux/index.js
+++ 16.vanilla-redux/index.js
@@ -6,3 +6,12 @@ const btnDecrease = document.querySelector('#decrease');
 const TOGGLE_SWITCH = 'TOGGLE_SWITCH';
 const INCREASE = 'INCREASE';
 const DECREASE = 'DECREASE';
+
+const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
+const increase = difference => ({ type: INCREASE, difference });
+const decrease = () => ({ type: DECREASE });
+
+const initialState = {
+  toggle: false,
+  counter: 0
+};
