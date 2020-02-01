7a03111dd55b331289a5edb8bf9fd1d49ccc982d
ifjso <ojsboy@hotmail.com>
2020-02-01 18:14:34 +0900

    provider에 store 연결

--- 17.react-redux-tutorial/src/index.js
+++ 17.react-redux-tutorial/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -8,7 +9,12 @@ import rootReducers from './modules';
 
 const store = createStore(rootReducers);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
