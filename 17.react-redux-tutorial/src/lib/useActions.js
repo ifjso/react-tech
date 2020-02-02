b0b6475ca4d9ec778b19865a70650e93ff23c7ec
ifjso <ojsboy@hotmail.com>
2020-02-02 20:46:19 +0900

    useActions 추가

---
+++ 17.react-redux-tutorial/src/lib/useActions.js
@@ -0,0 +1,19 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { bindActionCreators } from 'redux';
+import { useDispatch } from 'react-redux';
+import { useMemo } from 'react';
+
+export default (actions, deps) => {
+  const dispatch = useDispatch();
+
+  return useMemo(
+    () => {
+      if (Array.isArray(actions)) {
+        return actions.map(a => bindActionCreators(a, dispatch));
+      }
+
+      return bindActionCreators(actions, dispatch);
+    },
+    deps ? [dispatch, ...deps] : deps
+  );
+};
